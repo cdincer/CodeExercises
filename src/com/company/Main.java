8f488503e3f20152e7f9d13320fc119a15eeda5a
Can Dincer <dincercan@gmail.com>
2020-02-18 19:33:42 +0300

    CodeSignal AreaCutter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.IntStream;
@@ -9,43 +10,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        int[] inputArray = {6, 2, 3, 8};
-
-        int result = makeArrayConsecutive2(inputArray);
-
+String Test="abacabad";
+    char result=    firstNotRepeatingCharacter(Test);
         System.out.println(result);
 
 
     }
 
-    public static int makeArrayConsecutive2(int[] statues) {
-
-        int counter, former, latter;
-        counter = 0;
-for(int i=0; i<statues.length-1;i++)
-{
-    for(int j=0;j<statues.length-i-1;j++)
-    if(statues[j]>statues[j+1])
-    {
-        former=statues[j];
-        statues[j]=statues[j+1];
-        statues[j+1]=former;
-    }
-}
-        for (int i = 0; i < statues.length - 1; i++) {
-            former = statues[i];
-            latter = statues[i + 1];
-            if (former + 1 != latter) {
-                while(former<latter-1)
-                {
-                    former++;
-                    counter++;
-                }
-            }
+  public static  char firstNotRepeatingCharacter(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            //Start building values and occurences
+            count.put(c, count.getOrDefault(c, 0) + 1);
         }
 
-        return counter;
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return s.charAt(i);
+        }
+        return '_';
+
     }
 
 }
