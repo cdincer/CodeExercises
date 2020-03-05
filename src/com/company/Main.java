fb8843016d269c49f4a094bb20322398eec212a5
Can Dincer <dincercan@gmail.com>
2020-03-05 17:16:40 +0300

    LeetCode Longest Substring Without Repeating Characters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,57 +6,49 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-
-//        ListNode<Integer> l1= {3, 1, 2, 3, 4, 5};
-        Integer k1 = 3;
-        ListNode l1 = new ListNode(2);
-
-        ListNode l2 =new ListNode(4);
-        l1.next=l2;
-        ListNode l3 = new ListNode(3);
-        l2.next=l3;
-
-        ListNode l4 = new ListNode(5);
-        ListNode l5 = new ListNode(6);
-        l4.next=l5;
-
-        ListNode l6 = new ListNode(4);
-        l5.next=l6;
-
-
-
-        ListNode l7 = addTwoNumbers(l1, l4);
-        ListNode l8 = l7.next;
-        ListNode l9 = l8.next;
+String inner="pwwke";
+      int result=  lengthOfLongestSubstring(inner);
 
     }
 
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
+    public static int lengthOfLongestSubstring(String s) {
+
+        char[] CharArray = s.toCharArray();
+        Set<Character> result = new HashSet();
+        int CurrCounter=0;
+        int HighestCounter=0;
+
+        for(int i=0; i<CharArray.length; i++)
+        {
+            if(result.contains(CharArray[i]))
+            {
+                result = new HashSet();
+
+                if(CurrCounter>HighestCounter)
+                {
+                    HighestCounter=CurrCounter;
+                }
+                CurrCounter=0;
+
+            }
+            else
+            {
+
+                result.add(CharArray[i]);
+                CurrCounter++;
+            }
         }
-        if (carry > 0) {
-            curr.next = new ListNode(carry);
-        }
-        return dummyHead.next;
+
+
+        return HighestCounter;
     }
 
- public static class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) { val = x; }
- }
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 
 }
 
