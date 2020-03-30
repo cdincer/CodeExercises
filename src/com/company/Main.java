da47adc37a52ca2ab502d31854e53d9c5e8f628e
cdincer <dincercan@gmail.com>
2020-03-30 17:59:02 +0300

    Arcade CircleOfNumbers added to solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,143 +7,73 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [] inputArray = {1, 4, 10, 6, 2};
-String input ="64.233.161.00";
-        int[][] arr = {{7, 4, 0, 1}, 
-         {5, 6, 2, 2}, 
-         {6, 10, 7, 8}, 
-         {1, 4, 2, 0}};
-        int[][] arr2 = {{1, 1, 1}, 
-         {1, 7, 1}, 
-         {1, 1, 1}};
-        int[][] arr3=boxBlur(arr);
-//int sum = avoidObstacles(inputArray);
 
+        String XCoor,YCoor;
+        XCoor="A1";
+        YCoor="B2";
+boolean Result =chessBoardCellColor(XCoor,YCoor);
 
     }
 
 
-public static int[][] boxBlur(int[][] image) {
-    int[][] result = new int[image.length-2][image[0].length-2];
-    int counter=0;
-    for (int i = 1; i < image.length-1; i++) {
-        for (int j = 1; j < image[i].length-1; j++) {
-            result[i-1][j-1] = avg(image, i, j);
-            counter ++;
+ public static   boolean chessBoardCellColor(String cell1, String cell2) {
+        Boolean resut=false;
+        //  return Math.abs(cell1.charAt(0) - cell2.charAt(0)) % 2 == Math.abs(cell1.charAt(1) - cell2.charAt(1)) % 2;
+
+        String CellResult1 = BlackorWhite(cell1);
+        String CellResult2 = BlackorWhite(cell2);
+        System.out.println("Cell Result1 + "+ CellResult1);
+        System.out.println("Cell Result2 + "+ CellResult2);
+
+        if(CellResult1 == CellResult2)
+            resut=true;
+        else
+            resut = false;
+
+        return resut;
+    }
+
+
+    public static  String BlackorWhite(String Entry)
+    {
+        HashMap<Character,Integer> ChessBoard = new HashMap<Character,Integer>();
+        Integer TotalValue=0;
+        ChessBoard.put('A',1);
+        ChessBoard.put('B',2);
+        ChessBoard.put('C',3);
+        ChessBoard.put('D',4);
+        ChessBoard.put('E',5);
+        ChessBoard.put('F',6);
+        ChessBoard.put('G',7);
+        ChessBoard.put('H',8);
+        String DefaultPoint = "White";
+        Integer CellValue = ChessBoard.get(Entry.charAt(0));
+        Character FirstCharacter =  Entry.charAt(0);
+        if(FirstCharacter == 'A')
+        {
+            TotalValue = Integer.valueOf(Character.toString( Entry.charAt(1)));
+        }else
+        {
+            TotalValue = CellValue + (8* Integer.valueOf(Character.toString( Entry.charAt(1))));
+
         }
-    }
-    return result;
-}
 
-  public static  int avg(int[][] image, int x, int y) {
-        int sum = 0;
-        for (int i = x-1; i <= x+1; i++) {
-            for (int j = y-1; j <= y+1; j++) {
-                sum += image[i][j];
+        System.out.println("Total cell value "+ TotalValue);
+
+        for(int i=1;i<TotalValue;i++)
+        {
+            if(DefaultPoint == "Black")
+            {
+                DefaultPoint = "White";
+            } else
+            {
+                DefaultPoint = "Black";
             }
         }
-        return sum / 9;
+
+        Entry = DefaultPoint;
+        return Entry;
     }
-
-/*
-Test 1
-Input:
-
-image:
-[[1,1,1],
- [1,7,1],
- [1,1,1]]
-
-Expected Output:
-
-[[1]]
-
------
-Test 2
-Input:
-
-image:
-[[0,18,9],
- [27,9,0],
- [81,63,45]]
-
-Expected Output:
-
-[[28]]
------
-Test 3
-Input:
-
-image:
-[[36,0,18,9],
- [27,54,9,0],
- [81,63,72,45]]
-
-Expected Output:
-
-[[40,30]]
------
-Test 4
-Input:
-
-image:
-[[7,4,0,1],
- [5,6,2,2],
- [6,10,7,8],
- [1,4,2,0]]
-
-Expected Output:
-
-[[5,4],
- [4,4]]
- ------
- Test 5
- Input:
-
- image:
- [[36,0,18,9,9,45,27],
-  [27,0,54,9,0,63,90],
-  [81,63,72,45,18,27,0],
-  [0,0,9,81,27,18,45],
-  [45,45,27,27,90,81,72],
-  [45,18,9,0,9,18,45],
-  [27,81,36,63,63,72,81]]
-
- Expected Output:
-
- [[39,30,26,25,31],
-  [34,37,35,32,32],
-  [38,41,44,46,42],
-  [22,24,31,39,45],
-  [37,34,36,47,59]]
-
-
-*/
-
-
-
-
-public static    int avoidObstacles(int[] inputArray) {
-
-        int jump = 1;
-        boolean fail = true;
-
-        while(fail) {
-            jump++;
-            fail = false;
-            for(int i=0; i<inputArray.length; i++)
-                if(inputArray[i]%jump==0) {
-                    fail = true;
-                    break;
-                }
-        }
-
-        return jump;
-    }
-
-
-
-
 
 
 }
