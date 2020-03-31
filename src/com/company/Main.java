aafb199919c0c25b561e6f92652b256fec79977a
cdincer <dincercan@gmail.com>
2020-03-31 13:12:22 +0300

    Arcade depositProfit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,70 +11,27 @@ public class Main {
         String XCoor,YCoor;
         XCoor="A1";
         YCoor="B2";
-boolean Result =chessBoardCellColor(XCoor,YCoor);
+        int Result =  depositProfit(1, 100, 64);
+        System.out.println(Result);
 
     }
 
 
- public static   boolean chessBoardCellColor(String cell1, String cell2) {
-        Boolean resut=false;
-        //  return Math.abs(cell1.charAt(0) - cell2.charAt(0)) % 2 == Math.abs(cell1.charAt(1) - cell2.charAt(1)) % 2;
+ public static   int depositProfit(int deposit, int rate, int threshold) {
 
-        String CellResult1 = BlackorWhite(cell1);
-        String CellResult2 = BlackorWhite(cell2);
-        System.out.println("Cell Result1 + "+ CellResult1);
-        System.out.println("Cell Result2 + "+ CellResult2);
-
-        if(CellResult1 == CellResult2)
-            resut=true;
-        else
-            resut = false;
-
-        return resut;
-    }
-
-
-    public static  String BlackorWhite(String Entry)
-    {
-        HashMap<Character,Integer> ChessBoard = new HashMap<Character,Integer>();
-        Integer TotalValue=0;
-        ChessBoard.put('A',1);
-        ChessBoard.put('B',2);
-        ChessBoard.put('C',3);
-        ChessBoard.put('D',4);
-        ChessBoard.put('E',5);
-        ChessBoard.put('F',6);
-        ChessBoard.put('G',7);
-        ChessBoard.put('H',8);
-        String DefaultPoint = "White";
-        Integer CellValue = ChessBoard.get(Entry.charAt(0));
-        Character FirstCharacter =  Entry.charAt(0);
-        if(FirstCharacter == 'A')
+        int investedyears=0;
+        while(threshold>deposit)
         {
-            TotalValue = Integer.valueOf(Character.toString( Entry.charAt(1)));
-        }else
-        {
-            TotalValue = CellValue + (8* Integer.valueOf(Character.toString( Entry.charAt(1))));
+            deposit = deposit+((deposit/100)*rate);
 
+            if(deposit>threshold)
+                return investedyears;
+
+            investedyears++;
         }
 
-        System.out.println("Total cell value "+ TotalValue);
-
-        for(int i=1;i<TotalValue;i++)
-        {
-            if(DefaultPoint == "Black")
-            {
-                DefaultPoint = "White";
-            } else
-            {
-                DefaultPoint = "Black";
-            }
-        }
-
-        Entry = DefaultPoint;
-        return Entry;
+        return investedyears;
     }
-
 
 }
 
