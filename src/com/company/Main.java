733ed163db55847be45676e5e5ddd31add42b9bf
cdincer <dincercan@gmail.com>
2020-04-24 15:21:37 +0300

    Leetcode ContainsDuplicate,Intersection,RotateArray

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,33 +6,25 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        int[] inputArray = {1,3,2,4};
-
-
-        int Result =  arrayMaxConsecutiveSum(inputArray,3);
-        System.out.println(Result);
-
+        int[] inputArray = {1,2,3,4,5,6,7};
+         rotate(inputArray,3);
+         String aaa = Arrays.toString(inputArray);
+System.out.println(aaa);
     }
 
 
- public static    int arrayMaxConsecutiveSum(int[] inputArray, int k) {
-        int TempLargest=0;
-        int Container=0;
-        for(int i=0;i<inputArray.length-k;i++)
-        {
-            Container=0;
-            for(int j=i;j<i+k;j++)
-            {
-                Container+=inputArray[j];
-            }
-            if(Container> TempLargest)
-            {
-                TempLargest = Container ;
-            }
+    public static void rotate(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        int random=0;
+
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+            random =(i + k) % nums.length;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
         }
 
-
-        return TempLargest;
     }
 
 
