7b1467c25ed05eda9a5eb43df2bc50156e788636
cdincer <dincercan@gmail.com>
2020-04-29 12:41:03 +0300

    Leetcode FirstUniqueCharacterinaString second solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,60 +6,50 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        int[] inputArray = {1,2,3,4,5,6,7};
-        int[] inputArray2 = {8,9,9,9};
+        int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
+        int[] inputArray2 = {8, 9, 9, 9};
+        char[] reverser = {'H', 'e', 'l', 'l', 'o'};
+        char[] reverser2 = {'1', '2', '3', '4', '5'};
+        //     int[] inputArray3 =  plusOne(inputArray2);
+        //     String aaa = Arrays.toString(inputArray3);
+        String aaa = "testcase";
 
-        int[] inputArray3 =  plusOne(inputArray2);
-         String aaa = Arrays.toString(inputArray3);
-System.out.println(aaa);
+        firstUniqChar(aaa);
+        System.out.println(aaa);
     }
 
 
-    public static int[] plusOne(int[] digits) {
-        int Carry=0;
-        int i=2;
-        int[] digits2;
-        digits[digits.length-1] = digits[digits.length-1] +1;
+    public static int firstUniqChar(String s) {
 
-        if(digits[digits.length-1]==10)
-        {
-            Carry=1;
-            digits[digits.length-1]=0;
+        HashMap<Character, Integer> OItem = new HashMap<Character, Integer>();
+        int spot = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (OItem.containsKey(s.charAt(i))) {
 
-            while(Carry==1 && i<digits.length)
-            {
-                digits[digits.length-i]=digits[digits.length-i]+1;
-                if(digits[digits.length-i]<10)
-                {
-                    Carry=0;
+                OItem.remove(s.charAt(i));
+                OItem.put(s.charAt(i), i);
 
-                }else if(digits[digits.length-i]>=10)
-                {
-                    Carry=1;
-                    digits[digits.length-i]=0;
-                    i++;
-                }
+            } else {
+                spot = i;
+                OItem.put(s.charAt(i), i);
             }
-
-        }
-
-        if(digits[0]==0 )
-        {
-            digits2 = new int[digits.length+1];
-            digits2[0]=1;
-            for(int y=1;y<digits.length;y++)
-            {
-                digits2[y]=digits[y];
-            }
-            return digits2;
         }
 
 
+        Iterator<Map.Entry<Character, Integer>> it = OItem.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Character, Integer> pair = it.next();
 
-        //Get the last digit.Add one to that,see if its increases more than 1.
-        //use a while loop to check the others
 
-        return digits;
+            if (spot == -1)
+                spot = pair.getValue();
+
+            if (spot > pair.getValue())
+                spot = pair.getValue();
+        }
+
+
+        return spot;
     }
 
 
