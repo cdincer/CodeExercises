f7ee653a3390b0d5f2260ddafb1b472bfe1f7d28
cdincer <dincercan@gmail.com>
2020-05-08 17:10:45 +0300

    LeetCode reverseLinkedList,minIncrementUniqueArray

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,45 +18,54 @@ public class Main {
         //     String aaa = Arrays.toString(inputArray3);
         String aaa = "mississippi";
         String bbb = "pa";
-        int ccc= strStr(aaa,bbb);
+        int ccc= minIncrementForUnique3(inputArray2);
         System.out.println(ccc);
     }
 
 
-    public static int strStr(String haystack, String needle) {
-        int result=-1;
-        int needleL=needle.length();
-        int haystackL=haystack.length();
-        String test="";
-        result= haystack.indexOf(needle) !=-1 ? 0 : -1; //true
 
-        if(result==-1)
-            return -1;
-        else
-        {
-            for(int i=0;i<haystackL;i++)
-            {
-                StringBuilder strBuilder = new StringBuilder();
-                for(int y=0;y<needleL;y++)
-                {
-                    strBuilder.append(haystack.charAt(i+y));
-                }
 
-                test=strBuilder.toString();
-                if(test.equals(needle))
-                {
-                    result=i;
-                    return result;
-                }
 
-            }
+
+
+
+
+
+    public static int minIncrementForUnique3(int[] a) {
+        int[] freq = new int[70000];
+        int moves = 0;
+
+        for (int num : a)
+            freq[num]++;
+
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] <= 1)
+                continue;
+            int diff = freq[i] - 1;
+            moves += diff;
+            freq[i + 1] += diff;
         }
 
-
-        return result;
+        return moves;
     }
 
 
+
+    public int minIncrementForUnique4(int[] A) {
+        int N = A.length;
+        int[] count = new int[N*2];
+        for(int i=0; i<N; i++) {
+            count[A[i]]++;
+        }
+        int increments = 0;
+        for(int i=0; i<count.length; i++) {
+            if(count[i]>1) {
+                count[i+1] = count[i+1] + (count[i]-1);
+                increments = increments + (count[i]-1);
+            }
+        }
+        return increments;
+    }
 }
 
 
