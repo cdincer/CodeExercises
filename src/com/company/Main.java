2d0263712fbf81a099c2b7586207cc8402e24efd
cdincer <dincercan@gmail.com>
2020-05-20 19:51:15 +0300

    LeetCode 27,35,58,67,1451

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         String result="";
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
-        int[] inputArray2 = {8, 9, 9, 9};
+        int[] inputArray2 = {8, 9, 3,4};
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
         char[] reverser2 = {'1', '2', '3', '4', '5'};
         String[] items= {"flower","flow","flight"};
@@ -16,9 +16,9 @@ public class Main {
 
         //     int[] inputArray3 =  plusOne(inputArray2);
         //     String aaa = Arrays.toString(inputArray3);
-        String aaa = "{[]}";
+        String aaa = "\"Keep calm and code on\"\n";
         String bbb = "pa";
-         boolean baa= isValid(aaa);
+        int bbb2= removeElement(inputArray2,9);
         System.out.println(aaa);
     }
 
@@ -29,40 +29,15 @@ public class Main {
     // Hash table that takes care of the mappings.
 
 
-    public static  boolean isValid(String s) {
-         HashMap<Character, Character> mappings;
-
-        // Initialize hash map with mappings. This simply makes the code easier to read.
-
-        mappings = new HashMap<Character, Character>();
-        mappings.put(')', '(');
-        mappings.put('}', '{');
-        mappings.put(']', '[');
-
-        // Initialize a stack to be used in the algorithm.
-        Stack<Character> stack = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            // If the current character is a closing bracket.
-            if (mappings.containsKey(c)) {
-
-                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
-                char topElement = stack.empty() ? '#' : stack.pop();
-                char MapR = mappings.get(c);
-                // If the mapping for this bracket doesn't match the stack's top element, return false.
-                if (topElement != mappings.get(c)) {
-                    return false;
-                }
-            } else {
-                // If it was an opening bracket, push to the stack.
-                stack.push(c);
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-
-        // If the stack still contains elements, then it is an invalid expression.
-        return stack.isEmpty();
+        return i;
     }
 
 }
