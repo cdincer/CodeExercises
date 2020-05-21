975588806f1758f352a840c7fd2e00b75dd37617
cdincer <dincercan@gmail.com>
2020-05-21 18:03:38 +0300

    LeetCode 67,69

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,9 +16,9 @@ public class Main {
 
         //     int[] inputArray3 =  plusOne(inputArray2);
         //     String aaa = Arrays.toString(inputArray3);
-        String aaa = "\"Keep calm and code on\"\n";
-        String bbb = "pa";
-        int bbb2= removeElement(inputArray2,9);
+        String aaa = "1010";
+        String bbb = "1011";
+         bbb= addBinary(aaa,bbb);
         System.out.println(aaa);
     }
 
@@ -26,18 +26,19 @@ public class Main {
 
 
 
-    // Hash table that takes care of the mappings.
 
-
-    public static int removeElement(int[] nums, int val) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                nums[i] = nums[j];
-                i++;
-            }
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder(); //Google immutability or string vs stringbuilder if you don't know why we use this instead of regular string
+        int i = a.length() - 1, j = b.length() -1, carry = 0; //two pointers starting from the back, just think of adding two regular ints from you add from back
+        while (i >= 0 || j >= 0) {
+            int sum = carry; //if there is a carry from the last addition, add it to carry
+            if (j >= 0) sum += b.charAt(j--) - '0'; //we subtract '0' to get the int value of the char from the ascii
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2); //if sum==2 or sum==0 append 0 cause 1+1=0 in this case as this is base 2 (just like 1+9 is 0 if adding ints in columns)
+            carry = sum / 2; //if sum==2 we have a carry, else no carry 1/2 rounds down to 0 in integer arithematic
         }
-        return i;
+        if (carry != 0) sb.append(carry); //leftover carry, add it
+        return sb.reverse().toString();
     }
 
 }
