a6fc300b3839a87104d21a4e7db2f379aa6e3059
cdincer <dincercan@gmail.com>
2020-06-01 19:10:39 +0300

    CodeSignal 46,47

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         String result="";
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
         int[] inputArray2 = {8, 9, 3,4};
-        int[] inputArray3 = {3,3,4};
+        int[] inputArray3 = {5, 1, 3, 4, 1};
 
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
         char[] reverser2 = {'1', '2', '3', '4', '5'};
@@ -21,39 +21,8 @@ public class Main {
         String aaa = "1010";
         String bbb = "1011";
        //  bbb= convertToTitle(29);
-        int a= majorityElement(inputArray3);
         System.out.println(bbb);
     }
-    public static int majorityElement(int[] nums) {
-        int counter=0;
-        int keeper=0;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                counter++;
-                keeper=nums[i];
-                if(counter>nums.length/2)
-                {
-                    return nums[i];
-                }
-            }else
-            {
-                counter=0;
-                keeper=0;
-            }
-        }
-
-
-        return keeper;
-    }
-
-
-
-
-
-
 
 }
 
