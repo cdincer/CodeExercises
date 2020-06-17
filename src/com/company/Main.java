01b87427a3962ef434fbaa54823e3cbdf45b4b73
cdincer <dincercan@gmail.com>
2020-06-17 18:08:54 +0300

    Codesignal Core 18

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         String result="";
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
         int[] inputArray2 = {8, 9, 3,4};
-        int[] inputArray3 = {5, 1, 3, 4, 1};
+        int[] inputArray3 = {24, 85, 0};
 
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
         char[] reverser2 = {'1', '2', '3', '4', '5'};
@@ -21,39 +21,27 @@ public class Main {
         String aaa ="there are some (12) digits 5566 in this 770 string 239";
         String bbb = "ab-CDE-fg_hi";
        //  bbb= convertToTitle(29);
-        int[][]  ccc = spiralNumbers(6);
+        int  ccc = arrayPacking(inputArray3);
         System.out.println(bbb);
     }
-  public static int[][] spiralNumbers(int n) {
-      int[][] array = new int[n][n];
+  public static  int arrayPacking(int[] a) {
+      String M="";
+      for(int i=a.length-1;-1<i;i--)
+      {
+          String s=Integer.toBinaryString(a[i]);
+          for(int y=0;y<=8;y++)
+          {
+              if(s.length()<8)
+              {
+                  s=0+s;
+              }
+          }
+          M+=s;
+      }
+      int result = Integer.parseInt(M,2);
 
-      int left = 0;
-      int right = n - 1;
-      int top = 0;
-      int down = n - 1;
-
-   for(int counter=1;counter<n*n;)
-   {
-       for(int x=left;x<=right;x++)
-           array[top][x]=counter++;
-       top++;
-
-       for(int y=top;y<=down;y++)
-           array[y][right] = counter++;
-       right--;
-
-       for(int x=right;x>=left;x--)
-           array[down][x]=counter++;
-       down--;
-
-       for(int y=down;y>=top;y--)
-           array[y][left]=counter++;
-       left++;
-   }
-      return array;
+      return result;
   }
-
-
 
 
 
