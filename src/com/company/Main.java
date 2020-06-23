cc3f293a8bd72fb0d6dfccb08b3edd7720aae537
cdincer <dincercan@gmail.com>
2020-06-23 16:17:44 +0300

    Codesignal Core 34,38,41

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         String result="";
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
-        int[] inputArray2 = {8, 9, 3,4};
+        int[] inputArray2 = {7, 2, 2, 5, 10, 7};
         int[] inputArray3 = {24, 85, 0};
 
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
@@ -21,24 +21,55 @@ public class Main {
         String aaa ="there are some (12) digits 5566 in this 770 string 239";
         String bbb = "ab-CDE-fg_hi";
        //  bbb= convertToTitle(29);
-        int  ccc = rounders(2444);
-        System.out.println(bbb);
+          inputArray3 = replaceMiddle(inputArray2);
+
+
+          for(int i=0;i<inputArray3.length;i++)
+        System.out.println(inputArray3[i]);
     }
-   public static int rounders(int value) {
-       int t = 1;
-       while (value >= 10) {
-           t *= 10;
-           int r = 0;
-           if (value % 10 >= 5)
-               r = 1;
-           value /= 10;
-           value += r;
-           System.out.println("aaa "+ value);
+       public static int[] replaceMiddle(int[] arr) {
+           int[] c = new int[arr.length-1];
+           if(arr.length % 2 ==1)
+               return arr;
+
+           int j=0;
+           int middle = arr[arr.length/2] + arr[arr.length-arr.length/2-1];
+
+    System.out.println("aaaa " + c.length/2);
+
+           for(int i=0;i<arr.length;i++)
+           {
+
+
+               if( i != arr.length/2 || i != arr.length-arr.length/2  )
+               {
+                   c[j]=arr[i];
+                   j++;
+               }
+           }
+           c[c.length/2]=middle;
+
+           return c;
        }
-       return t * value;
-   }
 
 
+
+    int[] replaceMiddle2(int[] arr) {
+        int size = arr.length;
+        if(size%2!=0) {
+            return arr;
+        }
+        int[] res = new int[size-1];
+        int i =0;
+        for(int j=0; j<size; j++) {
+            if(j==size/2-1) {
+                res[i++] = arr[j] + arr[j+1];
+            } else if(j != size/2) {
+                res[i++] = arr[j];
+            }
+        }
+        return res;
+    }
 
 
 
