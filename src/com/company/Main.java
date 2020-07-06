f54de799df61688327fbe262ab8a12e0368ce8bc
cdincer <dincercan@gmail.com>
2020-07-06 16:50:32 +0300

    Leetcode 520,1089,1260

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
         int[] inputArray2 = {7, 2, 2, 5, 10, 7};
         int[] inputArray3 = {24, 85, 0};
-        int[] inputArray4 = {1,1,2,2,3,3};
+        int[] inputArray4 = {1,0,2,3,0,4,5,0};
 
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
         char[] reverser2 = {'1', '2', '3', '4', '5'};
@@ -23,7 +23,7 @@ public class Main {
         //     String aaa = Arrays.toString(inputArray3);
         String aaa ="there are some (12) digits 5566 in this 770 string 239";
         String bbb = "ab-CDE-fg_hi";
-        int ccc = distributeCandies(inputArray4);
+        duplicateZeros(inputArray4);
        //  bbb= convertToTitle(29);
 
     }
@@ -34,13 +34,43 @@ public class Main {
 
 
 
-    public static      int distributeCandies(int[] candies) {
-        Set<Integer> set = new HashSet<>();
-        for(int i =0 ; i< candies.length; i++){
-            set.add(candies[i]);
+    public static          void duplicateZeros(int[] arr) {
+        int possibleDups = 0;
+        int length_ = arr.length - 1;
+
+        // Find the number of zeros to be duplicated
+        // Stopping when left points beyond the last element in the original array
+        // which would be part of the modified array
+        for (int left = 0; left <= length_ - possibleDups; left++) {
+
+            // Count the zeros
+            if (arr[left] == 0) {
+
+                // Edge case: This zero can't be duplicated. We have no more space,
+                // as left is pointing to the last element which could be included
+                if (left == length_ - possibleDups) {
+                    // For this zero we just copy it without duplication.
+                    arr[length_] = 0;
+                    length_ -= 1;
+                    break;
+                }
+                possibleDups++;
+            }
         }
 
-        return Math.min(set.size(), candies.length/2);
+        // Start backwards from the last element which would be part of new array.
+        int last = length_ - possibleDups;
+
+        // Copy zero twice, and non zero once.
+        for (int i = last; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i + possibleDups] = 0;
+                possibleDups--;
+                arr[i + possibleDups] = 0;
+            } else {
+                arr[i + possibleDups] = arr[i];
+            }
+        }
     }
 
 
