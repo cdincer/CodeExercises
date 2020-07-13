7905516e3bb63b567eb61aa5aef9a0161c96b77e
cdincer <dincercan@gmail.com>
2020-07-13 18:14:31 +0300

    Leetcode 598,599

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,21 +10,21 @@ public class Main {
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
         int[] inputArray2 = {7, 2, 2, 5, 10, 7};
         int[] inputArray3 = {24, 85, 0};
-        int[] inputArray4 = {1,3,2,2,5,2,3,7
-};
+        int[] inputArray4 = {1,3,2,2,5,2,3,7};
+        int[][] Marr =  {{2,2,3},{3,4,3}};
 
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
         char[] reverser2 = {'1', '2', '3', '4', '5'};
-        String[] items= {"flower","flow","flight"};
-        String[] items2= {"aa","aa"};
+        String[] items= {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] items2= {"KFC", "Shogun", "Burger King"};
 
-        String s ="";
+
 
         //     int[] inputArray3 =  plusOne(inputArray2);
         //     String aaa = Arrays.toString(inputArray3);
         String aaa ="there are some (12) digits 5566 in this 770 string 239";
         String bbb = "ab-CDE-fg_hi";
-        findLHS(inputArray4);
+        findRestaurant(items,items2);
        //  bbb= convertToTitle(29);
 
     }
@@ -35,18 +35,22 @@ public class Main {
 
 
 
-    public static     int findLHS(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0) + 1);
-        }
-        int res = 0;
-        for(int key : map.keySet()){
-            if(map.containsKey(key + 1)){
-                res = Math.max(res,map.get(key) + map.get(key + 1));
+    public static    String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap < Integer, List < String >> map = new HashMap < > ();
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    if (!map.containsKey(i + j))
+                        map.put(i + j, new ArrayList < String > ());
+                    map.get(i + j).add(list1[i]);
+                }
             }
         }
-        return res;
+        int min_index_sum = Integer.MAX_VALUE;
+        for (int key: map.keySet())
+            min_index_sum = Math.min(min_index_sum, key);
+        String[] res = new String[map.get(min_index_sum).size()];
+        return map.get(min_index_sum).toArray(res);
     }
 
 
