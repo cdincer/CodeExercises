ced4179fc54b8ae9794ca1e6bb4111a24daa30b9
cdincer <dincercan@gmail.com>
2020-07-24 16:52:10 +0300

    Leetcode 665

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,50 +6,33 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        String result="";
+        String result = "";
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7};
         int[] inputArray2 = {7, 2, 2, 5, 10, 7};
         int[] inputArray3 = {24, 85, 0};
-        int[] inputArray4 = {1,3,1,3,100};
-        int[][] Marr =  {{2,2,3},{3,4,3}};
+        int[] inputArray4 = {3,4,2,3};
+        int[][] Marr = {{2, 2, 3}, {3, 4, 3}};
 
         char[] reverser = {'H', 'e', 'l', 'l', 'o'};
         char[] reverser2 = {'1', '2', '3', '4', '5'};
-        String[] items= {"Shogun", "Tapioca Express", "Burger King", "KFC"};
-        String[] items2= {"KFC", "Shogun", "Burger King"};
-
+        String[] items = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] items2 = {"KFC", "Shogun", "Burger King"};
 
 
         //     int[] inputArray3 =  plusOne(inputArray2);
         //     String aaa = Arrays.toString(inputArray3);
-        String aaa ="there are some (12) digits 5566 in this 770 string 239";
+        String aaa = "there are some (12) digits 5566 in this 770 string 239";
         String bbb = "ab-CDE-fg_hi";
-    double ccc =    rob(inputArray4);
-       //  bbb= convertToTitle(29);
-
-    }
-
-
-
-
-
-    public static  int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
-    }
-
-    private static int rob(int[] num, int lo, int hi) {
-        int include = 0, exclude = 0;
-        for (int j = lo; j <= hi; j++) {
-            int i = include, e = exclude;
-            include = e + num[j];
-            exclude = Math.max(e, i);
+        //  bbb= convertToTitle(29);
+        for (int i = 0; i < inputArray4.length; i++) {
+            System.out.println(inputArray4[i]);
         }
-        return Math.max(include, exclude);
-    }
 
+    }
 
 
 }
+
+
 
 
