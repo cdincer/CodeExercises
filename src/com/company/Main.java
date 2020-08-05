368cfdc9e071c26c56fb41c6e86b374643cf14d4
cdincer <dincercan@gmail.com>
2020-08-05 15:38:23 +0300

    CodeSignal 49,50,54,55

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,18 +17,44 @@ public class Main {
         char[] reverser2 = {'1', '2', '3', '4', '5'};
         String[] items = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
         String[] items2 = {"KFC", "Shogun", "Burger King"};
-
+         String plandrome = "ZZzzabzZzz";
 
         //     int[] inputArray3 =  plusOne(inputArray2);
         //     String aaa = Arrays.toString(inputArray3);
         String aaa = "there are some (12) digits 5566 in this 770 string 239";
         String bbb = "ab-CDE-fg_hi";
         //  bbb= convertToTitle(29);
+        boolean ccccc = isCaseInsensitivePalindrome(plandrome);
         for (int i = 0; i < inputArray4.length; i++) {
             System.out.println(inputArray4[i]);
         }
 
     }
+
+  public static  boolean isCaseInsensitivePalindrome(String inputString) {
+      int reversecounter=inputString.length()-1;
+      int counter=0;
+      inputString = inputString.toUpperCase();
+      for(int i=0;i<inputString.length()/2+1;i++)
+      {
+          Character a = inputString.charAt(i);
+          Character b = inputString.charAt(reversecounter-i);
+
+          if(a != b && i!= inputString.length() /2 )
+          {
+              counter++;
+              System.out.print(inputString.charAt(i));
+          }
+
+          if(counter>1 )
+          {
+              return false;
+          }
+
+      }
+
+      return true;
+  }
 
 
 }
